8b21b8419745cfa015735bd5b3c3347baf26442f
kushalchh07 <kushalchhetri54@gmail.com>
2024-10-20 20:38:46 +0545

    feat:  day 4

---
+++ python104.py
@@ -0,0 +1,40 @@
+# Python Indentation 
+"""
+    Indentation refers to the spaces at the beginning of code
+    where python looks for statement and loops
+    ( There should be at least one space at the beginning of line)
+    for example 
+
+"""
+''' if 5 > 2:
+    print("Five is greater than two!")
+     # Above code will not produce any error
+if 5 > 2:
+print("Five is greater than two!")
+
+# Here above code will produce error
+'''
+
+# Comments in Python
+# Single Line Comments
+
+# This is a single line comment
+
+# Multi Line Comments   
+"""
+This is a multi line comment
+written in
+more than just one line
+""" 
+# Python Variables
+"""
+Variables do not need to be declared with any particular type, and can even change type after they have been set. 
+"""
+x=5 #int
+y="Kushal"#string
+print(x)
+print(y)
+
+
+
+
