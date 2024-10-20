cd48f2b8af029389d84db84c047c5b6c846c8146
kushalchh07 <kushalchhetri54@gmail.com>
2024-10-20 20:46:02 +0545

    python variables, comments, multiline comments, assigning multiple variables, casting

--- python104.py
+++ python104.py
@@ -37,4 +37,29 @@ print(y)
 
 
 
+# Casting = If you want to specify the data type of a variable, this can be done with casting.
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+"""
+    A variable can have a short name (like x and y) or a more descriptive name (age, carname, total_volume). Rules for Python variables:
+A variable name must start with a letter or the underscore character
+A variable name cannot start with a number
+A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+Variable names are case-sensitive (age, Age and AGE are three different variables)
+A variable name cannot be any of the Python keywords.
 
+
+    """
+    
+    # Assign Multiple values
+
+"""
+    Many Values to Multiple Variables
+Python allows you to assign values to multiple variables in one line:
+
+"""
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
