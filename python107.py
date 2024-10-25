822aa7a49d1f58d3b953cb5ac60c89fe419fd951
kushalchh07 <kushalchhetri54@gmail.com>
2024-10-25 22:03:20 +0545

    feat: tuples in python

--- python107.py
+++ python107.py
@@ -1,6 +1,7 @@
 #DATA STRUCTURES IN PYTHON
 
 # Lists in python
+"""
 names =["Kushal","Rohit","Ram","Shyam"]
 print(names) 
 print(names[0])
@@ -34,3 +35,41 @@ print(names) #replaces kushal with pranita
 
 names.clear() #clears the list
 print(names)
+"""
+#Tuples
+"""
+thistuple = ("apple", "banana", "cherry")
+print(thistuple)
+
+thistuple = ("apple", "banana", "cherry", "apple", "cherry")
+print(thistuple)
+
+thistuple = ("apple", "banana", "cherry")
+print(len(thistuple))
+
+thistuple = ("apple", "banana", "cherry")
+print(thistuple[1])
+
+thistuple = ("apple", "banana", "cherry")
+print(thistuple[-1])
+
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[2:5])
+
+thistuple = ("apple", "banana", "cherry")
+if "apple" in thistuple:
+  print("Yes, 'apple' is in the fruits tuple") 
+  """
+
+#update tuples
+thistuple = ("apple", "banana", "cherry")
+y = ("orange",)
+thistuple += y
+print(thistuple)
+
+#delete tuples  
+thistuple = ("apple", "banana", "cherry")
+del thistuple
+print(thistuple)
+
+ 
