2d4024d43c5b81f1b103f1c747a56ca38474c320
kushalchh07 <kushalchhetri54@gmail.com>
2024-10-25 22:17:28 +0545

    feat: join tuples

--- python107.py
+++ python107.py
@@ -112,6 +112,7 @@ print(yellow)
 print(red)
 """
 #loop through tuples
+"""
 
 thistuple = ("apple", "banana", "cherry")
 for x in thistuple:
@@ -128,3 +129,17 @@ while i < len(thistuple):
 thistuple = ("apple", "banana", "cherry")
 for i in range(len(thistuple)):
   print(thistuple[i])
+  
+  """
+  
+  #join tuples
+tuple1 = ("a", "b" , "c")
+tuple2 = (1, 2, 3)
+
+tuple3 = tuple1 + tuple2
+print(tuple3)
+
+#multiplying tuples
+fruits = ("apple", "banana", "cherry")
+mytuple = fruits * 2
+print(mytuple)
